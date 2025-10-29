33ec72b824eec830bcb10171f01a91364d6f64a0
romloj <58185116+romloj@users.noreply.github.com>
2025-10-29 22:50:39 +0100

    Update app.py

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ print("--- SERWER ZALADOWAL NAJNOWSZA WERSJE PLIKU (POSPRZĄTANĄ) ---")
 TZ = ZoneInfo("Europe/Warsaw")
 
 # Inicjalizacja aplikacji Flask
-app = Flask(__name_
+app = Flask(__name_)
 CORS(app)
 app.config['DEBUG'] = True
 
